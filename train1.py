214ad8cf7febdcd7af64e995c440e0f36b654bc5
ariya__bk <karykar83@gmail.com>
2026-02-03 14:35:09 +0330

    first clean code

--- train1.py
+++ train1.py
@@ -1,10 +1,9 @@
 age = int(input ('please enter your age between 0 to 100 : '))
 if age <=0 or age >=100 :
   print('wrong age')
-else:  
-  if age < 18 :
-    print ('limited access')
-  if age >= 18 and age < 60:
-    print ('welcome baby')
-  if age >= 60 :
-    print ('be careful ezraiil right behind you')
+elif age < 18 :
+  print ('limited access')
+elif 18<= age <60 :
+  print ('welcome baby')
+elif age >= 60 :
+    print ('be careful Ezraiil right behind you')
