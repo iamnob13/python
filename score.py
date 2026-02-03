0c5f71372b25b46cff67a6113a0aa259047a2bcc
ariya__bk <karykar83@gmail.com>
2026-02-03 22:15:23 +0330

    fix bugs

--- score.py
+++ score.py
@@ -1,13 +1,15 @@
 score_list = []
 for i in range(3) :
-  name = input('Please enter your "name" : ')
-  score = input(f'Please enter {name} "score" : ')
-  try :
-    score = int(score)
-    score_list.append(score)
-  except :
-    print('Error, Please enter the correct score : ')
-    continue
+  name = input('"name" : ')
+  while True :    # وایل یه لوپ میسازه که مجبورش کنه
+    score = input(f'"Score" for {name} : ')
+    score = score.strip()
+    try :
+      score = float(score)
+      score_list.append(score)
+      break
+    except ValueError:
+      print('Error, Please enter the correct score : ')
 total_sum = sum(score_list)
 avg = sum(score_list)/len(score_list)
 print('Total sum : ',total_sum)
