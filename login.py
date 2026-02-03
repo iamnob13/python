529fff94920f908dd55abbdd1aaf68dea2a198f1
ariya__bk <karykar83@gmail.com>
2026-02-03 16:19:19 +0330

    feat new code

--- login.py
+++ login.py
@@ -1,7 +1,10 @@
 allowed_users = ['ariya-bk','gilda','mamad']
+block_users = ['aliwch','cj','hamilton']
 user = input ('enter your username to login : ')
-user = user.lower().strip() #متد  ها واسه یوزر نیم
-if user in allowed_users :
-  print ('OK, welcome back my love')
+user = user.lower().strip()
+if user in block_users :
+  print ('error, Your username is blocked !')
+elif user in allowed_users :
+  print ('fine, welcome back my love')
 else :
-  print ('Your username was wrong,Please try again')  
+  print ('error, Your username was wrong,Please try again')
