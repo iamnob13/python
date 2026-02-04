9cc3d9d85531f7eba8def57e745490d20bb0969e
ariya__bk <karykar83@gmail.com>
2026-02-04 14:10:52 +0330

    fix name

--- login.py
+++ login.py
@@ -1,10 +1,10 @@
 allowed_users = ['ariya-bk','gilda','mamad']
 block_users = ['aliwch','cj','hamilton']
-user = input ('enter your username to login : ')
-user = user.lower().strip()
+user = input ('username : ')
+user = user.lower().strip() 
 if user in block_users :
-  print ('error, Your username is blocked !')
+  print ('error, username blocked !')
 elif user in allowed_users :
-  print ('fine, welcome back my love')
+  print ('Login confirm')
 else :
-  print ('error, Your username was wrong,Please try again')
+  print ('error, username wrong')
