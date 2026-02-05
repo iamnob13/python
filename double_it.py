1ddaa1a30314f54eac6fe0e91bf8865eea7c8918
ariya__bk <karykar83@gmail.com>
2026-02-05 14:01:42 +0330

    just a little function

--- double_it.py
+++ double_it.py
@@ -1,12 +1,14 @@
+def get_number(num) :
+  while True :
+    number = input(num)
+    try :
+      number = float(number)
+      return number
+    except ValueError:
+      print('value error')  
 def double_it (a) :
   double = a * 2
-  return double 
-while True :
-  number = input('Number : ')
-  try :
-    number = float(number)
-    break
-  except ValueError:
-    print('value error')
-javab = double_it(number)
-print(javab)
+  return double
+number = get_number('Number : ')
+number = double_it(number)
+print(f'Double it ==> {number}')
