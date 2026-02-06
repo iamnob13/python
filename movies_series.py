fabeafccd3fedd99d331d3f846decd06460de36f
ariya__bk <karykar83@gmail.com>
2026-02-06 18:35:48 +0330

    first commit for movies and series archive

---
+++ movies_series.py
@@ -0,0 +1,12 @@
+my_archive = {
+  ######### Genres should bi in list & Rating should be integer چون مثلا فیلمای بالاتر از 9 
+  'Breaking_bad' : {'Title':'Breaking bad','Year':2008,'Genres':['Drama'],'Rating':9.5,'Is_series':True},
+  'Inception' : {'Title':'Inception','Year':2010,'Genres':['Sci_fi','Action'],'Rating':8.8,'Is_series':False},
+  'Whiplash' : {'Title':'Whiplash','Year':2014,'Genres':['Drama','Indie'],'Rating':8.5,'Is_series':False}
+}
+#print(my_archive['Whiplash']['Year'])
+#print(my_archive['Whiplash']['Genres'][0])
+#my_archive['Breaking_bad']['Is_series'] = False
+#print(my_archive['Breaking_bad']['Is_series'])
+
+
