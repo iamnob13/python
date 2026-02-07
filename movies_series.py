eea749848d7857fa65fa94ec2d6df3d833a5f416
ar!ya_bk <karykar83@gmail.com>
2026-02-07 20:06:08 +0330

    Clean code

--- movies_series.py
+++ movies_series.py
@@ -1,22 +1,15 @@
+# یه تمرین کوچیک دیکشنری ها و دیکشنری تودرتو شامل لیست | قراره تکمیل بشه
 my_archive = {
   ######### Genres should bi in list & Rating should be integer چون مثلا فیلمای بالاتر از 9 
   'Breaking_bad' : {'Title':'Breaking bad','Year':2008,'Genres':['Drama'],'Rating':9.5,'Is_series':True},
   'Inception' : {'Title':'Inception','Year':2010,'Genres':['Sci_Fi','Action'],'Rating':8.8,'Is_series':False},
   'Whiplash' : {'Title':'Whiplash','Year':2014,'Genres':['Drama','Indie'],'Rating':8.5,'Is_series':False}
 }
-#========NOTHING==================#
-#print(my_archive['Whiplash']['Year'])
-#print(my_archive['Whiplash']['Genres'][0])
-#my_archive['Breaking_bad']['Is_series'] = False
-#print(my_archive['Breaking_bad']['Is_series'])
-#========NOTHING==================#
-
 print('==========')
 for i in my_archive.keys() :
   imdb = my_archive[i]['Rating']
   if imdb >= 8.6 :
     print(f'{i} IMDB ===> {imdb}')
-
 
 print('==========')
 print('Movies : ')
@@ -25,7 +18,6 @@ for i in my_archive.keys() :
   movie = my_archive[i]['Is_series']
   if movie ==False :
     print(i)
-
 
 def get_genre (answer) :
   answer = answer.title().strip()
