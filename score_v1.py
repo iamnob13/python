64289005fa369e310152d0cb923111c8b012f864
ariya__bk <karykar83@gmail.com>
2026-02-08 19:12:18 +0330

    change

--- score_v1.py
+++ score_v1.py
@@ -1,16 +1,34 @@
-score_list = []
-for i in range(3) :
-  name = input('"name" : ')
-  while True :    # وایل یه لوپ میسازه که مجبورش کنه
-    score = input(f'"Score" for {name} : ')
-    score = score.strip()
+# this code get  3 name and 3 score 
+# tell you if some one Failed
+def get_score(text) :
+  while True :
+    score = input(text)
     try :
       score = float(score)
-      score_list.append(score)
-      break
+      if 0<= score <=20 :
+        return score
+      else :
+        print('value error')
     except ValueError:
-      print('Error, Please enter the correct score : ')
-total_sum = sum(score_list)
-avg = sum(score_list)/len(score_list)
-print('Total sum : ',total_sum)
-print('Score average : ',avg)
+      print('value error')
+def get_name(text) :
+  while True :
+    name = input(text)
+    name = name.strip()
+    if len(name) == 0 :
+      print('Name error')
+    else :  
+      name = name.title()
+      return name
+all_name =[]
+all_score =[]
+for i in range(3) :
+  all_name.append(get_name('Name : '))
+  all_score.append(get_score('Score : '))
+for i in range(len(all_score)) :
+  name = all_name[i]
+  score = all_score[i]
+  if score < 10 :
+    print(f'{name} Failed')
+  else :
+    print(f'{name} Passed')
