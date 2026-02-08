824a6bd9d460ed5c3d274f0976f1bb692923a492
ariya__bk <karykar83@gmail.com>
2026-02-08 20:32:03 +0330

    just training kist & dict

--- score_v2.py
+++ score_v2.py
@@ -1,34 +1,49 @@
-# this code get  3 name and 3 score 
-# tell you if some one Failed
-def get_score(text) :
-  while True :
-    score = input(text)
-    try :
-      score = float(score)
-      if 0<= score <=20 :
-        return score
-      else :
-        print('value error')
-    except ValueError:
-      print('value error')
-def get_name(text) :
-  while True :
-    name = input(text)
-    name = name.strip()
-    if len(name) == 0 :
-      print('Name error')
-    else :  
-      name = name.title()
-      return name
-all_name =[]
-all_score =[]
-for i in range(3) :
-  all_name.append(get_name('Name : '))
-  all_score.append(get_score('Score : '))
-for i in range(len(all_score)) :
-  name = all_name[i]
-  score = all_score[i]
-  if score < 10 :
-    print(f'{name} Failed')
-  else :
-    print(f'{name} Passed')
+# از بین تمام اطلاعات و لیست ها و دیکشنری ها نام و رشته ی کسانی که نمره بالاتر از 17 دارند را در یک فایل دیگر ذخیره کند
+import json
+try :
+  with open('course.json','r') as file :
+    data = json.load(file)
+  
+  my_list = []
+  for first in data :
+    for second in first['students'] :
+      final_grade = (second['grade'])
+      if final_grade > 17 :
+        javab = (first['title'],second)
+        my_list.append(javab)
+  with open('top_score.json','w') as my_file :
+    json.dump(my_list,my_file,indent=4)
+  print('Done')
+except FileNotFoundError:
+  print('File Not Found Error')
+
+# [
+#   {
+#     "course_id": 101,
+#     "title": "Python Basics",
+#     "students": [
+#       {
+#         "name": "Ali",
+#         "grade": 18
+#       },
+#       {
+#         "name": "Sara",
+#         "grade": 15
+#       }
+#     ]
+#   },
+#   {
+#     "course_id": 102,
+#     "title": "Django Deep Dive",
+#     "students": [
+#       {
+#         "name": "Reza",
+#         "grade": 20
+#       },
+#       {
+#         "name": "Mina",
+#         "grade": 12
+#       }
+#     ]
+#   }
+# ]
