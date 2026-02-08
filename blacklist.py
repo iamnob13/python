6c8e591f6b63a4c56e4391c8460700f69075e1e1
ariya__bk <karykar83@gmail.com>
2026-02-08 17:07:24 +0330

    just training file handling

--- blacklist.py
+++ blacklist.py
@@ -1,7 +1,42 @@
-block_users = ['aliwch','cj','hamilton']
-user = input('enter your username : ')
-user = user.lower().strip()
-if user in block_users :
-  print ('Your username is blocked !')
+import json
+
+block_users = ['Aliwch','Cj','Hamilton']
+
+def save() :
+  with open('blacklist.json','w') as file:
+    json.dump(block_users,file,indent=5)
+
+def read():
+  try :
+    with open('blacklist.json','r') as file :
+      data = json.load(file)
+      return data
+  except FileNotFoundError:
+    save()
+    return block_users    
+
+def con(name) :
+  while True :
+    answer = input(f'Add {name} to block list y/n : ')
+    answer = answer.lower().strip()
+    if answer == 'y' :
+      return True
+    elif answer == 'n' :
+      return False
+
+block_users = read()
+
+
+name = input('User name : ')
+name = name.title().strip()
+if name in block_users :
+  print('Access Denied')
 else :
-  print ('All fine')  
+  if con(name) :
+    block_users.append(name)
+    print('================')
+    print('New list : ')
+    print(block_users)
+    save()
+  else :
+    print('Bye')
