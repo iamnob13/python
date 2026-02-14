99aa8774c828ef3fd5b9d206beab0777bc26ea99
ariya__bk <karykar83@gmail.com>
2026-02-14 16:44:19 +0330

    feat .json

--- class_4.py
+++ class_4.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Product :
   def __init__(self,name,price,stock):
     self.name = name
@@ -12,6 +15,25 @@ class Product :
   def get_total_value (self) :
     javab = self.price * self.stock
     return javab 
+
+
+
+class Fased(Product) :
+  def __init__(self, name, price, stock,expired_date):
+    super().__init__(name, price, stock)
+    self.expired_date = expired_date
+  def show (self) :
+    print(f'{self.name} value ===> {self.get_total_value()}')
+    print(f'{self.name} Expired date ===> {self.expired_date}')
+  def is_available (self) :
+    if self.expired_date > '2026-05-10' :
+      return False
+    else :
+      if self.stock > 0 :
+        return True
+      else :
+        print('Mojood nist')
+
 
 class Warehouse :
   def __init__(self,warehouse_name):
@@ -57,13 +79,25 @@ class Warehouse :
       print('Transferred ok')
     else :
       print('Kala nist')
-
+try:
+  with open('my_product.json','r') as file :
+    my_file = json.load(file)
+  my_data = []
+  for i in my_file :
+    if 'expired_date' in i :
+      obj = Fased(i['name'],i['price'],i['stock'],i['expired_date'])
+      my_data.append(obj)
+    else : 
+      obj = Product(i['name'],i['price'],i['stock'])
+      my_data.append(obj)
+except FileNotFoundError :
+  print('File Not Found Error')
 
 p1 = Product('apple',3,33)
 p2 = Product('phone',100,25)
 p3 = Product('shoes',10,50)
 p4 = Product('banana',2,78)
-p5 = Product('milk',1,100)
+p5 = Fased('milk',1,100,'2026-05-11')
 p6 = Product('laptop',200,0)
 my_list = [p1,p2,p3,p4,p5,p6]
 if p6.is_available() :
@@ -84,3 +118,23 @@ mamad_warehouse.add_product(p4)
 print(f'Most Expensive  Product ==> {ariya_warehouse.find_expensive()}')
 ariya_warehouse.transfer_to('phone',gilda_warehouse)
 print(f'All Gilda balance ==> {gilda_warehouse.inventory_report()}')
+p5.show()
+javab = p5.is_available()
+if javab :
+  print(f'{p5.name} is available')
+else :
+  print(f'{p5.name} Expired')
+
+finally_list = []
+for j in my_list :
+  my_product = {
+    'name':j.name,
+    'price':j.price,
+    'stock':j.stock
+  }
+  if hasattr(j,'expired_date') :
+    my_product['expired_date'] = j.expired_date
+  finally_list.append(my_product)
+with open('my_product.json','w') as file :
+  json.dump(finally_list,file,indent=4)
+
